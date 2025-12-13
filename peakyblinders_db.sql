4e0b9c2250ae27a5b2cd673a1ab296082fc08449
aljatsiyaprofitar <al.jatsiya.p@mhs.unsoed.ac.id>
2025-12-13 16:16:09 +0700

    Update branch ke 2

--- peakyblinders_db.sql
+++ peakyblinders_db.sql
@@ -13,6 +13,24 @@ CREATE TABLE users (
 CREATE TABLE characters (
     char_id INT(20) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    quotes VARCHAR(255) NOT NULL
+    role VARCHAR (100),
+    short_desc TEXT,
+    long_desc TEXT,
+    quotes TEXT,
+    image_url VARCHAR(255)
+);
+
+CREATE TABLE timelline (
+    time_id INT(20) AUTO_INCREMENT PRIMARY KEY,
+    character_id INT NOT NULL,
+    year_period VARCHAR(50),
+    event_description TEXT,
+    display_order INT DEFAULT 0,
+    FOREIGN KEY (character_id) REFERENCES characters(char_id) ON DELETE CASCADE
+);
+
+CREATE TABLE rp_char (
+    rp_char_id INT(20) AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
+    timeline VARCHAR(300) NOT NULL
 );
